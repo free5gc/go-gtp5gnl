0555cd6d4cd0a7d145b5b8b93d94e95f528aea6a
Tim Liu <timliu@nctu.edu.tw>
2023-06-29 02:04:35 +0000

    refactor code

--- attr_report.go
+++ attr_report.go
@@ -2,6 +2,7 @@ package gtp5gnl
 
 import (
 	"time"
+	"unsafe"
 
 	"github.com/khirono/go-nl"
 )
@@ -66,69 +67,46 @@ type VolumeMeasurement struct {
 // The maximun netlink message size is 16K, and the body for the attibutes are 7856 Bytes
 const (
 	MAX_NETLINK_MSG_BODY_SIZE = 7856
+	NETLIMK_ATTR_HDR_SIZE     = 4
 )
 
 // The netlink attribute size of UR need to count the UR header(4) + size of the attributes (and it's header) in UR
-func URSize() int {
-	hdr := 0
-	body := 0
+func URnlAttrSize() int {
+	size := NETLIMK_ATTR_HDR_SIZE         // UR attr header
 
-	if UR > 0 {
-		hdr += 4
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_URRID attr header
+	size += int(unsafe.Sizeof(uint32(0))) // UR_URRID attr data
 
-		if UR_URRID > 0 {
-			hdr += 4
-			body += 4
-		}
-		if UR_USAGE_REPORT_TRIGGER > 0 {
-			hdr += 4
-			body += 4
-		}
-		if UR_URSEQN > 0 {
-			hdr += 4
-			body += 4
-		}
-		if UR_VOLUME_MEASUREMENT > 0 {
-			hdr += 4
-			if UR_VOLUME_MEASUREMENT_TOVOL > 0 {
-				hdr += 4
-				body += 8
-			}
-			if UR_VOLUME_MEASUREMENT_UVOL > 0 {
-				hdr += 4
-				body += 8
-			}
-			if UR_VOLUME_MEASUREMENT_DVOL > 0 {
-				hdr += 4
-				body += 8
-			}
-			if UR_VOLUME_MEASUREMENT_TOPACKET > 0 {
-				hdr += 4
-				body += 8
-			}
-			if UR_VOLUME_MEASUREMENT_UPACKET > 0 {
-				hdr += 4
-				body += 8
-			}
-			if UR_VOLUME_MEASUREMENT_DPACKET > 0 {
-				hdr += 4
-				body += 8
-			}
-		}
-		if UR_START_TIME > 0 {
-			hdr += 4
-			body += 8
-		}
-		if UR_END_TIME > 0 {
-			hdr += 4
-			body += 8
-		}
-		if UR_SEID > 0 {
-			hdr += 4
-			body += 8
-		}
-	}
-	return hdr + body
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_USAGE_REPORT_TRIGGER attr header
+	size += int(unsafe.Sizeof(uint32(0))) // UR_USAGE_REPORT_TRIGGER attr data
+
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_URSEQN attr header
+	size += int(unsafe.Sizeof(uint32(0))) // UR_URSEQN attr data
+
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT attr header
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_TOVOL attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_TOVOL attr data
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_UVOL attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_UVOL attr data
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_DVOL attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_DVOL attr data
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_TOPACKET attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_TOPACKET attr data
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_UPACKET attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_UPACKET attr data
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_VOLUME_MEASUREMENT_DPACKET attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_VOLUME_MEASUREMENT_DPACKET attr data
+
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_START_TIME attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_START_TIME attr data
+
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_END_TIME attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_END_TIME attr data
+
+	size += NETLIMK_ATTR_HDR_SIZE         // UR_SEID attr header
+	size += int(unsafe.Sizeof(uint64(0))) // UR_SEID attr data
+
+	return size
 }
 
 func decodeVolumeMeasurement(b []byte) (VolumeMeasurement, error) {
